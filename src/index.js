6767b77e00762a7e78f42156f6cca775c01e1455
mbernallaura <mbernallaura@gmail.com>
2022-10-06 19:23:27 -0500

    Se realiza el primer acercamiento con la promesa tipo fetch( ) y tips cuando se encadenan varias promesas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-//Importar los archivos que queremos utilizar para que pueda funcionar el actual archivo 
-import {saludar} from './js/componentes.js';
-import './styles.css';
 
-const nombre = 'laura';
+const jokeUrl = 'https://api.chucknorris.io/jokes/random'
 
-saludar(nombre);
+//*Errores que se pueden generar en http:
+//? 200 = Todo lo hizo bien
+//? 201 = el registro se creo
+//? 404 = No encontró el recurso
+//? 500 = problemas del servidor
+
+// fetch(jokeUrl).then(resp =>{
+//     resp.json().then(({id, value}) =>{
+//         console.log(id);
+//         console.log(value);
+//     });
+// })
+
+//*Hacer lo anterior de una mejor manera 
+//?Se puede acortar de esa manera porque es una funcion de flecha que solo tiene una instruccion
+fetch(jokeUrl)
+    .then(resp => resp.json())
+    .then(({id, value}) =>{
+        console.log(id,value);
+    });
